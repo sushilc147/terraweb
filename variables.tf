6259502eb06047d870465db466d57313c0bf5be8
swan <35875553+sushilc147@users.noreply.github.com>
2020-05-14 13:03:35 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,6 +12,6 @@
 
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
-  type        = number
-  default     = 8080
+  type        = "number"
+  default     = "8080"
 }
