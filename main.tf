5c2850c498cf295a8c69a449c856fe02d33573d4
swan <35875553+sushilc147@users.noreply.github.com>
2020-05-14 13:00:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_instance" "example" {
   # Ubuntu Server 18.04 LTS (HVM), SSD Volume Type in us-east-2
   ami                    = "ami-0c55b159cbfafe1f0"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  vpc_security_group_ids = "aws_security_group.instance.id"
 
   user_data = <<-EOF
               #!/bin/bash
